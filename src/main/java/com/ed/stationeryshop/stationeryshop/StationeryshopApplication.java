2064fd194f7b2f30a524fb17687992a7fe3487a1
trunglearn <trungnqworkit@gmail.com>
2024-05-07 15:48:57 +0700

    connect database

---
+++ src/main/java/com/ed/stationeryshop/stationeryshop/StationeryshopApplication.java
@@ -0,0 +1,13 @@
+package com.ed.stationeryshop.stationeryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationeryshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationeryshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ed/stationeryshop/stationeryshop/StationeryshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed.stationeryshop.stationeryshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationeryshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
